0e4b07a1be0f4daeff9bdf2483267e0c9aa85e90
Luis Miguel <luismiguelgarciadelasmorenas@gmail.com>
2025-08-30 20:23:44 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,20 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'screens/home_screen.dart';
 
-void main() => runApp(CerebrixApp());
+void main() => runApp(const CerebrixApp());
 
 class CerebrixApp extends StatelessWidget {
+  const CerebrixApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Cerebrix',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: WelcomeScreen(),
-    );
-  }
-}
-
-class WelcomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurple[50],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('🧠 Cerebrix', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-            SizedBox(height: 20),
-            Text('Desafía tu mente cada día con retos únicos y divertidos. ¡Todo 100% gratis!',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18)),
-            SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Comenzar'),
-            ),
-          ],
-        ),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const HomeScreen(),
     );
   }
 }
